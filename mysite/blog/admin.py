73e437546b286fe8e9a26c3ab07195899794c9c6
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-03-17 12:07:36 +0300

    Настройка способов отображения в админ панеле

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,7 +3,16 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
+    """
+    https://runebook.dev/ru/docs/django/ref/contrib/admin/index#django.contrib.admin.ModelAdmin.list_filter
+    """
     list_display = ['title', 'author', 'created', 'updated', 'status']
+    list_filter = ('status', 'created', 'publish',)
+    search_fields = ("title", 'body',)
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    odrering = ['status', 'publish']
 
 
 admin.site.register(Post, PostAdmin)
