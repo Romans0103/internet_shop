9d1f07fb7a890c7a940db96eae9c7ad9db77b1cd
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-03-17 12:29:09 +0300

    Формирование списка и detail

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,12 +7,12 @@ class PostAdmin(admin.ModelAdmin):
     https://runebook.dev/ru/docs/django/ref/contrib/admin/index#django.contrib.admin.ModelAdmin.list_filter
     """
     list_display = ['title', 'author', 'created', 'updated', 'status']
-    list_filter = ('status', 'created', 'publish',)
+    list_filter = ('status', 'created', 'published',)
     search_fields = ("title", 'body',)
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
-    date_hierarchy = 'publish'
-    odrering = ['status', 'publish']
+    date_hierarchy = 'published'
+    odrering = ['status', 'published']
 
 
 admin.site.register(Post, PostAdmin)
